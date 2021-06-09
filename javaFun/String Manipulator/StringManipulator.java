49cde58634f4cff78416cd91f132d3020a4ee8e6
asmaalasaada <asma.alsaada96@gmail.com>
2021-06-09 18:12:12 +0300

    String Manipulation

---
+++ javaFun/String Manipulator/StringManipulator.java
@@ -0,0 +1,24 @@
+public class StringManipulator{
+    // static String trimAndConcat(String str1, String str2){
+    //     String tr1=str1.trim();
+    //     String tr2=str2.trim();
+    //     String c=tr1.concat(tr2);
+    //     return c;
+    // }
+    // static Integer getIndexOrNull(String word, char letter){
+
+    //     int a = word.indexOf(letter);
+    //     return a;
+    // }
+    // static Integer getIndexOrNull(String word, String subString){
+    //     Integer a = word.indexOf(subString);
+    //     Integer b = word.indexOf(subString);
+    //     return a;
+    // }
+    // static String concatSubstring(String word, int num1, int num2, String str2){
+    //     String c=word.substring(num1,num2);
+    //     // String str5=str2.concat(c); 
+    //     String str5=c.concat(str2);
+    //     return str5;
+    // }
+}
