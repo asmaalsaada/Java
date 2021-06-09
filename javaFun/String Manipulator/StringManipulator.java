62f0e4a0ac721542e15a8726f943806f4e3acdb1
asmaalasaada <asma.alsaada96@gmail.com>
2021-06-09 18:39:11 +0300

    String Manipulation

--- javaFun/String Manipulator/StringManipulator.java
+++ javaFun/String Manipulator/StringManipulator.java
@@ -5,16 +5,22 @@ public class StringManipulator{
     //     String c=tr1.concat(tr2);
     //     return c;
     // }
-    // static Integer getIndexOrNull(String word, char letter){
+    static Integer getIndexOrNull(String word, char letter){
 
-    //     int a = word.indexOf(letter);
-    //     return a;
-    // }
-    // static Integer getIndexOrNull(String word, String subString){
-    //     Integer a = word.indexOf(subString);
-    //     Integer b = word.indexOf(subString);
-    //     return a;
-    // }
+        int a = word.indexOf(letter); 
+            if (a==-1){
+                return null;
+            }
+        return a;
+     }
+    static Integer getIndexOrNull(String word, String subString){
+        Integer a = word.indexOf(subString);
+        Integer b = word.indexOf(subString);
+    if (a==-1){
+        return null;
+    }
+        return a;
+    }
     // static String concatSubstring(String word, int num1, int num2, String str2){
     //     String c=word.substring(num1,num2);
     //     // String str5=str2.concat(c); 
